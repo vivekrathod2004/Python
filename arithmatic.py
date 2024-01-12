ff053f19828233ca14b2a3d806cc50d1b63ff66d
Vivek Rathod <132133152+vivekrathod2004@users.noreply.github.com>
2024-01-12 13:03:24 +0530

    Create arithmatic.py

---
+++ arithmatic.py
@@ -0,0 +1,20 @@
+a = int(input("Enter first num: "))
+b = int(input("Enter second num: "))
+
+c = a + b
+print(f"Addition of two numbers is {c}")
+
+d = a - b
+print(f"Subtraction of two numbers is {d}")
+
+e = a * b
+print(f"Multiplication of two numbers is {e}")
+
+f = a / b
+print(f"Division of two numbers is {f}")
+
+i = a ** b
+print(f"Exponentiation of two numbers is {i}")
+
+f = a // b
+print(f"Floor division of two numbers is {f}")
